a4dbb003e575c21b6fbf01f2c1577aa1fb530f8a
nicolas <nhomble@terpmail.umd.edu>
2020-07-02 02:29:06 -0400

    narrow the exception

--- src/test/java/io/github/nhomble/netty/nonproxyhostissue/NonProxyHostsTest.java
+++ src/test/java/io/github/nhomble/netty/nonproxyhostissue/NonProxyHostsTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.Test;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.ProxyProvider;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.regex.PatternSyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NonProxyHostsTest {
 
@@ -15,7 +18,7 @@ public class NonProxyHostsTest {
         HttpClient client = HttpClient
                 .create()
                 .tcpConfiguration(c -> c.proxy(po -> po.type(ProxyProvider.Proxy.HTTP).host("forward.proxy").nonProxyHosts(NON_PROXY_HOSTS)));
-        RuntimeException e = assertThrows(RuntimeException.class, () -> client.get().uri("127.0.0.1").response().block());
+        PatternSyntaxException e = assertThrows(PatternSyntaxException.class, () -> client.get().uri("127.0.0.1").response().block());
         String msg = e.getMessage();
         assertTrue(msg.contains("Dangling meta character '*' near index 0"));
         assertTrue(msg.contains("*.some.domain"));
